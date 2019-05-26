45ef0a39de5f22dfde416e3f900e784e8bd6a116
Lior Subotnick <subotnil@wvhs.wlwv.k12.or.us>
2019-05-25 22:46:47 -0700

    Account Name Input Added
    Added Account Name Input
    Now outputs multiple passwords in a table

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,23 @@
 var password = "";
-
 function generatePassword() {
-    var length = "8";
-    password = "";
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        password += characters.charAt(Math.floor(Math.random() * charactersLength));
+    if (event.keyCode == 13) {
+        var accountName = document.getElementById("account").value;
+        var length = "8";
+        password = "";
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            password += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        document.getElementById("password-display").style.display = "block";
+        document.getElementById("copy-button").style.display = "block";
+
+        var table = document.getElementById("password-display");
+        var row = table.insertRow(0);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = accountName;
+        cell2.innerHTML = password;
+        document.getElementById("account").value = '';
     }
-    document.getElementById("password-display").innerHTML = password
-    document.getElementById("password-display").style.display = "block";
 }
